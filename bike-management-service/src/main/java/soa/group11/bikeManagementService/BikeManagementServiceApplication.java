c29dead2d2bf2e1603a3d5ef7b174fc8398cf6b5
Cristina Racovita <cristina.racovita98@gmail.com>
2024-03-21 15:57:43 +0100

    add bike management service project

---
+++ bike-management-service/src/main/java/soa/group11/bikeManagementService/BikeManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package soa.group11.bikeManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeManagementServiceApplication.class, args);
+	}
+
+}
---
+++ bike-management-service/src/test/java/soa/group11/bikeManagementService/BikeManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package soa.group11.bikeManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
