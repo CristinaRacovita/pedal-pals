9dae60ae1f942062404368d74770bf84f443cfb4
Bindila-Bogdan <b.mihaibogdan@yahoo.com>
2024-03-26 11:55:22 +0100

    initialize rental request service

---
+++ rental-request-service/src/test/java/soa/group11/rentalRequestService/RentalRequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package soa.group11.rentalRequestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalRequestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
