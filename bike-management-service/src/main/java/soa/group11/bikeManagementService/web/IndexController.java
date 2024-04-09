c72551aca8dcf4600813acd8cd7d2793ccbf4749
Cristina Racovita <cristina.racovita98@gmail.com>
2024-04-09 20:22:09 +0200

    add header

--- bike-management-service/src/main/java/soa/group11/bikeManagementService/web/IndexController.java
+++ bike-management-service/src/main/java/soa/group11/bikeManagementService/web/IndexController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import soa.group11.bikeManagementService.models.BikeCardDto;
 import soa.group11.bikeManagementService.services.BikeService;
@@ -15,14 +14,6 @@ import soa.group11.bikeManagementService.services.BikeService;
 public class IndexController {
     @Autowired
     private BikeService bikeService;
-
-    // @GetMapping(value = "/bikes/{userId}/{role}")
-    // public String getBikesForUser(Model model, @PathVariable(value = "userId") int userId,
-    //         @PathVariable(value = "role") String role) {
-    //     model.addAttribute("bikes", bikeService.getBikesByUserId(userId));
-    //     model.addAttribute("role", role);
-    //     return "not_implemented";
-    // }
 
     @GetMapping(value = "/bikes")
     public String getBikes(Model model) {
