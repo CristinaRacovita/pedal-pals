7a7d0a965ad0a9561ddc50d256181545c02882b5
Bindila-Bogdan <b.mihaibogdan@yahoo.com>
2024-04-09 19:38:51 +0200

    fix bug related to no args constructor

--- rental-service/src/main/java/soa/group11/rentalService/models/DetailedRentalApprovalDto.java
+++ rental-service/src/main/java/soa/group11/rentalService/models/DetailedRentalApprovalDto.java
@@ -1,10 +1,12 @@
 package soa.group11.rentalService.models;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class DetailedRentalApprovalDto {
     private String requesterId;
     private String bikeId;
