552ee4b1916bfccf154296cf2f836a8f0cb74696
Bindila-Bogdan <b.mihaibogdan@yahoo.com>
2024-04-10 23:23:33 +0200

    include rentals service

--- rental-service/src/main/java/soa/group11/rentalService/services/RentalApprovalService.java
+++ rental-service/src/main/java/soa/group11/rentalService/services/RentalApprovalService.java
@@ -1,17 +1,13 @@
 package soa.group11.rentalService.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import soa.group11.rentalService.entities.RentalApproval;
 import soa.group11.rentalService.entities.RentalRequest;
-import soa.group11.rentalService.models.BikeDto;
 import soa.group11.rentalService.models.RentalApprovalDto;
 import soa.group11.rentalService.repositories.RentalApprovalRepository;
 import soa.group11.rentalService.repositories.RentalRequestRepository;
@@ -23,51 +19,6 @@ public class RentalApprovalService {
 
     @Autowired
     private RentalRequestRepository rentalRequestRepository;
-
-    public List<BikeDto> getBikesRentedIn(String userId) {
-        List<BikeDto> bikesRentedIn = new ArrayList<>();
-
-        List<RentalApprovalDto> approvals = rentalApprovalRepository.findAll().stream()
-                .map(rentalApproval -> toRentalApprovalDto(rentalApproval))
-                .collect(Collectors.toList());
-
-        List<RentalRequest> requestsForOwner = rentalRequestRepository.findAllByBikeOwnerId(userId);
-
-        for (RentalRequest rentalRequest : requestsForOwner) {
-            BikeDto bike = getBikeByBikeId(rentalRequest.getBikeId());
-
-            if (bike != null){
-                bike.setPersonOfContact(rentalRequest.getBikeRequesterId());
-                bike.setRequestStatus(rentalRequest.getStatus());
-    
-                for (RentalApprovalDto approval : approvals) {
-                    if (approval.getRequestId().equals(rentalRequest.getId())) {
-                        bike.setApprovalStatus(approval.getApprovalStatus());
-                    }
-                }
-                bikesRentedIn.add(bike);
-            }
-        }
-
-        return bikesRentedIn;
-    }
-
-    private BikeDto getBikeByBikeId(String bikeId) {
-        try {
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<BikeDto> response = restTemplate.getForEntity(
-                    "http://localhost:8082/bike/" + bikeId, BikeDto.class);
-
-            BikeDto bikeDto = response.getBody();
-
-            return bikeDto;
-
-        } catch (Exception e) {
-            System.out.println("Bike not found...");
-            return null;
-        }
-
-    }
 
     public List<RentalApprovalDto> getApprovalStatuses() {
         return rentalApprovalRepository.findAll().stream().map(rentalApproval -> toRentalApprovalDto(rentalApproval))
