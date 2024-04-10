2b9367b10a406db62f71034989afbc54a5395629
Bindila-Bogdan <b.mihaibogdan@yahoo.com>
2024-04-10 15:27:18 +0200

    send dummy bike data

--- bike-management-service/src/main/java/soa/group11/bikeManagementService/producers/NotificationProducer.java
+++ bike-management-service/src/main/java/soa/group11/bikeManagementService/producers/NotificationProducer.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import soa.group11.bikeManagementService.models.BikeDto;
+import soa.group11.bikeManagementService.models.BikeSubscriptionDto;
 
 @Component
 public class NotificationProducer {
@@ -20,8 +21,11 @@ public class NotificationProducer {
 
     public void sendMessage(BikeDto bike) {
         try {
+            // TODO: send real data
+            BikeSubscriptionDto bikeSubscriptionDto = new BikeSubscriptionDto("Cristina", 27, 1, "2022-04-09 12:00", "2023-01-10 11:00", "Union", "Regular", "City");
+
             ObjectMapper mapper = new ObjectMapper();
-            String bikeJson = mapper.writeValueAsString(bike);
+            String bikeJson = mapper.writeValueAsString(bikeSubscriptionDto);
             jmsTemplate.convertAndSend(notificationQueue, bikeJson);
 
         } catch (JsonProcessingException e) {
