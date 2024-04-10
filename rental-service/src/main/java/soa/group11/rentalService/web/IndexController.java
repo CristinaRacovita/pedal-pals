a4a6e50bd7a4c91da180580d2d754917b6a89b66
Bindila-Bogdan <b.mihaibogdan@yahoo.com>
2024-04-10 23:22:47 +0200

    test rental service

--- rental-service/src/main/java/soa/group11/rentalService/web/IndexController.java
+++ rental-service/src/main/java/soa/group11/rentalService/web/IndexController.java
@@ -3,30 +3,26 @@ package soa.group11.rentalService.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import soa.group11.rentalService.models.BikeDto;
-import soa.group11.rentalService.services.RentalApprovalService;
-import soa.group11.rentalService.services.RentalRequestService;
+import soa.group11.rentalService.services.RentalsService;
 
 @RestController
 public class IndexController {
     @Autowired
-    private RentalApprovalService rentalApprovalService;
-
-    @Autowired
-    private RentalRequestService rentalRequestService;
+    private RentalsService rentalsService;
 
     @GetMapping(value = "/{userId}/my-bikes")
     public List<BikeDto> getBikes(@PathVariable(value = "userId") String userId) {
-        //model.addAttribute("rented_in_bikes", rentalApprovalService.getBikesRentedIn(userId));
-        //model.addAttribute("rented_out_bikes", rentalRequestService.getBikesRentedOut(userId));
+        // model.addAttribute("rented_in_bikes",
+        // rentalApprovalService.getBikesRentedIn(userId));
+        // model.addAttribute("rented_out_bikes",
+        // rentalRequestService.getBikesRentedOut(userId));
 
-        //return "user_rentals";
-        return rentalApprovalService.getBikesRentedIn(userId);
+        // return "user_rentals";
+        return rentalsService.getBikeRentals(userId, false);
     }
 }
