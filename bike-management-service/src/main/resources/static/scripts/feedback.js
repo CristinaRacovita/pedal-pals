97ba998b7ed76d628745a026ce114032afdd5dc3
Cristina Racovita <cristina.racovita98@gmail.com>
2024-04-19 16:21:22 +0200

    use cookies for storing userId

--- bike-management-service/src/main/resources/static/scripts/feedback.js
+++ bike-management-service/src/main/resources/static/scripts/feedback.js
@@ -1,6 +1,3 @@
-function goToFeedback() {
-    var userId = document.getElementsByName('header')[0].id;
-    var bikeId = document.getElementsByName('bike-card')[0].id;
-
-    window.location.href = "http://localhost:8081/" + userId + "/rating/" + bikeId;
+function goToFeedback(bikeId) {
+    window.location.href = "http://localhost:8081/ratings/" + bikeId;
 }
